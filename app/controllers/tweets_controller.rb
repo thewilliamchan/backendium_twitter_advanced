3cbb261411d46ba22db52c223bee6fd940866fb9
Xiaopow <xiaoyang88@gmail.com>
2016-12-21 17:25:13 +0800

    add image posting functionality for frontend

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,7 +8,6 @@ class TweetsController < ApplicationController
     token = cookies.signed[:twitter_session_token]
     session = Session.find_by(token: token)
     user = session.user
-
     @tweet = user.tweets.new(tweet_params)
 
     if @tweet.save
