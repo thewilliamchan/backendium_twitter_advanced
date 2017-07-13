f2747e26c0cf7c6d8ac964e5285acb6200720d81
h8rry <harry@harrychen.net>
2017-07-13 17:28:31 +0800

    Add Image to Tweets

---
+++ db/migrate/20170713092440_add_image_columns_to_tweets.rb
@@ -0,0 +1,10 @@
+class AddImageColumnsToTweets < ActiveRecord::Migration[5.0]
+  def change
+    def up
+      add_attachment :tweets, :image
+    end
+    def down
+      remove_attachment :tweets, :image
+    end
+  end
+end
