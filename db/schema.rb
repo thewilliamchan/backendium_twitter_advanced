5604bbe1e0a6c5869cb03c3acf48857b1953b3b2
h8rry <harry@harrychen.net>
2017-07-13 17:28:31 +0800

    Add Image to Tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720043452) do
+ActiveRecord::Schema.define(version: 20170713092440) do
 
   create_table "sessions", force: :cascade do |t|
     t.string   "token"
