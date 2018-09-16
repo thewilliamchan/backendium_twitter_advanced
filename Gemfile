c364104093f741322133b61e5c1c6b7bb1621475
williamwlchan <william.wlchan@outlook.com>
2018-09-16 18:29:24 +0800

    update tweet model for s3 photo upload

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'paperclip', '~> 5.0.0'
 
 gem 'pg', '~> 0.21'
 
+gem 'aws-sdk'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,13 @@ class Tweet < ApplicationRecord
   validates :user, presence: true
   validates :message, presence: true, length: { maximum: 140 }
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  # has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+  has_attached_file :image, :storage => :s3, :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def s3_credentials
+    {:bucket => ENV['PHOTO_UPLOAD_BUCKET'], :access_key_id => ENV['AWS_S3_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_S3_SECRET_ACCESS_KEY'], :s3_region => "ap-northeast-1", :s3_host_name => "s3-ap-northeast-1.amazonaws.com"}
+  end
 end
